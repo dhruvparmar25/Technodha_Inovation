0b7137263c465c653ebf1d945ddc3fa4474fe726
dhruvparmar25 <parmardhruv034@gmail.com>
2026-03-25 16:08:45 +0530

    bg

--- src/sections/home/HeroSection.jsx
+++ src/sections/home/HeroSection.jsx
@@ -48,15 +48,15 @@ function HeroSection() {
     >
       {/* Background */}
       <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('/bg-hero.jpg')" }}
+        className="absolute inset-0 bg-cover opacity-25"
+        style={{ backgroundImage: "url('/bg-hero.jpg') " }}
       />
 
       {/* Overlay */}
-      <div className="absolute inset-0  backdrop-blur-[2px]" />
+      <div className="absolute inset-0 " />
 
       {/* Content */}
-      <div className="relative max-w-305 mx-auto w-full px-4 min-h-screen py-10 flex flex-col lg:flex-row items-center justify-between gap-10 ">
+      <div className="relative max-w-305 mx-auto w-full min-h-screen py-10 flex flex-col lg:flex-row items-center justify-between gap-10 ">
         {/* LEFT */}
         <div className="w-full lg:w-1/2 text-center lg:text-left mt-10 lg:mt-0 flex flex-col items-center lg:items-start">
           <h1
@@ -95,8 +95,8 @@ function HeroSection() {
           </div>
         </div>
         {/* RIGHT */}
-        <div className="w-full lg:w-1/2 flex justify-center relative">
-          <div className="relative z-10">
+        <div className="w-full lg:w-1/2 flex justify-end relative">
+          <div className="relative ">
             <Image
               src={Main}
               alt="Laptop"
